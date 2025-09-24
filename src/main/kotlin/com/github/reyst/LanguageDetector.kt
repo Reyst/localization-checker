8b55c571e42eb4256b45decc2266828b6ada27e4
Reyst <reyst@i.ua>
2025-09-24 12:56:55 +0300

    Refactor build scripts and update dependencies
    - Update Gradle wrapper to version 8.5
    - Update Kotlin plugin and standard library
    - Update Apache POI and Log4j dependencies
    - Add .gitignore file
    - Replace `sun.misc.Regexp` with `kotlin.text.Regex`

--- src/main/kotlin/com/github/reyst/LanguageDetector.kt
+++ src/main/kotlin/com/github/reyst/LanguageDetector.kt
@@ -1,15 +1,14 @@
 package com.github.reyst
 
-import sun.misc.Regexp
 import java.util.regex.Pattern
 
 class LanguageDetector {
 
-    private val regexp = Regexp(LANGUAGE_RESOURCE_PATTERN)
+    private val regexp = Regex(LANGUAGE_RESOURCE_PATTERN)
 
     fun getLanguageCode(folderName: String): String {
 
-        val pattern = Pattern.compile(regexp.exp)
+        val pattern = Pattern.compile(regexp.pattern)
         val regExpResult = pattern.matcher(folderName)
 
         if(regExpResult.matches()) {
